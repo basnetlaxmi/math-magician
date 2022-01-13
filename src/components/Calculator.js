f0291197afe70e170aba9979403d27cf869df674
basnetlaxmi <ddsanzel@gmail.com>
2022-01-14 01:38:01 +0545

    Add layout of calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -10,10 +10,71 @@ export default class Calculator extends React.Component {
         </div>
 
         <div className="button-box">
-          <div className="button-row" />
-          <div className="button-row" />
-          <div className="button-row" />
-          <div className="button-row" />
+          <div className="button-row">
+            <button type="button" className="button-column-1">
+              AC
+            </button>
+            <button type="button" className="button-column-2">
+              +/-
+            </button>
+            <button type="button" className="button-column-3">
+              %
+            </button>
+            <button type="button" className="button-column-4">
+              *
+            </button>
+          </div>
+          <div className="button-row">
+            <button type="button" className="button-column-1">
+              7
+            </button>
+            <button type="button" className="button-column-2">
+              8
+            </button>
+            <button type="button" className="button-column-3">
+              9
+            </button>
+            <button type="button" className="button-column-4">
+              x
+            </button>
+          </div>
+          <div className="button-row">
+            <button type="button" className="button-column-1">
+              4
+            </button>
+            <button type="button" className="button-column-2">
+              5
+            </button>
+            <button type="button" className="button-column-3">
+              6
+            </button>
+            <button type="button" className="button-column-4">
+              -
+            </button>
+          </div>
+          <div className="button-row">
+            <button type="button" className="button-column-1">
+              1
+            </button>
+            <button type="button" className="button-column-2">
+              2
+            </button>
+            <button type="button" className="button-column-3">
+              3
+            </button>
+            <button type="button" className="button-column-4">
+              +
+            </button>
+          </div>
+          <div className="button-row">
+            <button type="button" className="button-column-1">
+              0
+            </button>
+
+            <button type="button" className="button-column-4">
+              =
+            </button>
+          </div>
 
         </div>
 

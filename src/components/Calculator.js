64e658f70b05b6d0c9774b885b50fb6b6d9d5738
basnetlaxmi <ddsanzel@gmail.com>
2022-01-15 10:07:35 +0545

    use hook

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,95 +1,90 @@
 /* eslint-disable react/prefer-stateless-function */
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-export default class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.handleClick = this.handleClick.bind(this);
-  }
+const Calculator = () => {
+  const [state, NewState] = useState({});
 
-  handleClick(e) {
-    this.setState((state) => calculate(state, e.target.textContent));
-  }
+  const handleClick = (e) => {
+    NewState(calculate(state, e.target.textContent));
+  };
 
-  render() {
-    const { next, total } = this.state;
-    return (
-      <div className="calc-container">
-        <div className="screen">
-          {next || total || 0 }
+  const { next, total } = state;
+  return (
+    <div className="calc-container">
+      <div className="screen">
+        {next || total || 0 }
+      </div>
+      <div className="button-box">
+        <div className="button-row">
+          <button type="button" className="button-column" onClick={handleClick}>
+            AC
+          </button>
+          <button type="button" className="button-column" onClick={handleClick}>
+            +/-
+          </button>
+          <button type="button" className="button-column" onClick={handleClick}>
+            %
+          </button>
+          <button type="button" className="button-column orange" onClick={handleClick}>
+            ÷
+          </button>
         </div>
-        <div className="button-box">
-          <div className="button-row">
-            <button type="button" className="button-column" onClick={this.handleClick}>
-              AC
-            </button>
-            <button type="button" className="button-column" onClick={this.handleClick}>
-              +/-
-            </button>
-            <button type="button" className="button-column" onClick={this.handleClick}>
-              %
-            </button>
-            <button type="button" className="button-column orange" onClick={this.handleClick}>
-              ÷
-            </button>
-          </div>
-          <div className="button-row">
-            <button type="button" className="button-column" onClick={this.handleClick}>
-              7
-            </button>
-            <button type="button" className="button-column" onClick={this.handleClick}>
-              8
-            </button>
-            <button type="button" className="button-column" onClick={this.handleClick}>
-              9
-            </button>
-            <button type="button" className="button-column orange" onClick={this.handleClick}>
-              x
-            </button>
-          </div>
-          <div className="button-row">
-            <button type="button" className="button-column" onClick={this.handleClick}>
-              4
-            </button>
-            <button type="button" className="button-column" onClick={this.handleClick}>
-              5
-            </button>
-            <button type="button" className="button-column" onClick={this.handleClick}>
-              6
-            </button>
-            <button type="button" className="button-column orange" onClick={this.handleClick}>
-              -
-            </button>
-          </div>
-          <div className="button-row">
-            <button type="button" className="button-column" onClick={this.handleClick}>
-              1
-            </button>
-            <button type="button" className="button-column" onClick={this.handleClick}>
-              2
-            </button>
-            <button type="button" className="button-column" onClick={this.handleClick}>
-              3
-            </button>
-            <button type="button" className="button-column orange" onClick={this.handleClick}>
-              +
-            </button>
-          </div>
-          <div className="button-row">
-            <button type="button" className="button-column-1" onClick={this.handleClick}>
-              0
-            </button>
+        <div className="button-row">
+          <button type="button" className="button-column" onClick={handleClick}>
+            7
+          </button>
+          <button type="button" className="button-column" onClick={handleClick}>
+            8
+          </button>
+          <button type="button" className="button-column" onClick={handleClick}>
+            9
+          </button>
+          <button type="button" className="button-column orange" onClick={handleClick}>
+            x
+          </button>
+        </div>
+        <div className="button-row">
+          <button type="button" className="button-column" onClick={handleClick}>
+            4
+          </button>
+          <button type="button" className="button-column" onClick={handleClick}>
+            5
+          </button>
+          <button type="button" className="button-column" onClick={handleClick}>
+            6
+          </button>
+          <button type="button" className="button-column orange" onClick={handleClick}>
+            -
+          </button>
+        </div>
+        <div className="button-row">
+          <button type="button" className="button-column" onClick={handleClick}>
+            1
+          </button>
+          <button type="button" className="button-column" onClick={handleClick}>
+            2
+          </button>
+          <button type="button" className="button-column" onClick={handleClick}>
+            3
+          </button>
+          <button type="button" className="button-column orange" onClick={handleClick}>
+            +
+          </button>
+        </div>
+        <div className="button-row">
+          <button type="button" className="button-column-1" onClick={handleClick}>
+            0
+          </button>
 
-            <button type="button" className="button-column orange" onClick={this.handleClick}>
-              =
-            </button>
-          </div>
-
+          <button type="button" className="button-column orange" onClick={handleClick}>
+            =
+          </button>
         </div>
 
       </div>
-    );
-  }
-}
+
+    </div>
+  );
+};
+export default Calculator;
